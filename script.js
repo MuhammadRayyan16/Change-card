6237874c9256f198650c906ececb3c46a3ac54e0
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-04-27 05:56:03 -0700

    Myjavascript

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// function card1change(cardone) {
-//    cardone.src = "/Assets/jackg1.jpeg"
-// }
-// function card1out(cardone) {
-//    cardone.src = "/Assets/jack1.jpeg"
-// }
+function card1change(cardone) {
+   cardone.src = "/Assets/jackg1.jpeg"
+}
+function card1out(cardone) {
+   cardone.src = "/Assets/jack1.jpeg"
+}
 
-// function card2change(cardtwo) {
-//    cardtwo.src = "/Assets/jackg2.jpeg"
-// }
-// function card2out(cardtwo) {
-//    cardtwo.src = "/Assets/jack2.jpeg"
-// }
+function card2change(cardtwo) {
+   cardtwo.src = "/Assets/jackg2.jpeg"
+}
+function card2out(cardtwo) {
+   cardtwo.src = "/Assets/jack2.jpeg"
+}
 
-// function card3change(cardthree) {
-//    cardthree.src = "/Assets/jackg3.jpeg"
-// }
-// function card3out(cardthree) {
-//    cardthree.src = "/Assets/jack3.jpeg"
-// }
+function card3change(cardthree) {
+   cardthree.src = "/Assets/jackg3.jpeg"
+}
+function card3out(cardthree) {
+   cardthree.src = "/Assets/jack3.jpeg"
+}
 
